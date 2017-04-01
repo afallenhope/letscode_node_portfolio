355087eb2b39abcf9304713d81bc236301c242cc
afallenhope <afallenhope@gmail.com>
2017-04-01 19:10:15 -0400

    Added our views, and more middleware

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express") // import our Express module
 const app = express() // instantiate an Expres app.
+const path = require("path")
 const mainRouter = require("./routes/default") // import our default routes .
-const port = process.env.PORT || 9999;
+const port = process.env.PORT || 9999
 
 app
-  .use("/", mainRouter)
+  .set("views", "./views")  // sets the view folder to ./views
+  .set("view engine", "ejs")  // use our EJS templating engine
+
+  .use("/css", express.static(path.join(__dirname,"./assets/css"))) // look in our assets/css when search for /css
+  .use("/js", express.static(path.join(__dirname,"./assets/js"))) // look in our assets/js when search for /js
+  .use("/fonts",express.static(path.join(__dirname,"./assets/fonts"))) // look in our assets/fonts when we request /fonts
+  .use("/images",express.static(path.join(__dirname,"./assets/images"))) // look in our assets/fonts when we request /fonts
+  
+  .use("/css",express.static(path.join(__dirname,"./node_modules/bootstrap/dist/css"))) // look in our assets/fonts when we request /fonts
+  .use("/js",express.static(path.join(__dirname,"./node_modules/bootstrap/dist/js"))) // look in our assets/fonts when we request /fonts
+  .use("/fonts",express.static(path.join(__dirname,"./node_modules/bootstrap/dist/fonts"))) // look in our assets/fonts when we request /fonts
+  
+  .use("/css",express.static(path.join(__dirname,"./node_modules/font-awesome/css"))) // look in our assets/fonts when we request /fonts
+  .use("/fonts",express.static(path.join(__dirname,"./node_modules/font-awesome/fonts"))) // look in our assets/fonts when we request /fonts
+
+  
+  
+  .use("/", mainRouter) // creates a path router to module ./routes/default.js  
+  // listen on port 9999 
   .listen(port, function(err,req,res){
-    console.log("Listening http://127.0.0.1:" + port);
+    console.log("Listening http://127.0.0.1:" + port)
   })
--- routes/default.js
+++ routes/default.js
@@ -1,10 +1,11 @@
 const express = require("express") // import express
-const router  = express.Router();
+const router  = express.Router() // create a router can also be called by doing required("express").Router();
+ 
 
 router.get("/", (req,res,next) => {
-  res.send("Hello World! ");
-  res.end();
-});
+  res.send("Hello World! ")
+  res.end()
+})
 
 
-module.exports = router;
+module.exports = router
